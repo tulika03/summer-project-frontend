45c1527f3b9e58cc37bfb07d8b6157152fea684f
tulika03 <tulika312@gmail.com>
2018-06-14 16:44:43 +0530

    category addtion

---
+++ src/components/admin/category/addCategory.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-spacer></v-spacer>
+
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field v-model="category_id" label="Category Id" type="text" required class="text--darken-1" :rule="idRules"></v-text-field>
+                  <v-text-field v-model="category_title"  label="Title" type="text" required :rules="titleRules"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Add</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+<script>
+  import axios from 'axios'
+  import Vue from 'vue'
+  import VueLocalStorage from 'vue-localstorage'
+  Vue.use(VueLocalStorage)
+  export default {
+    name: 'app',
+    data () {
+      return {
+        category_id: '',
+        category_title: '',
+        idRules: [
+          v => !!v || 'category Id is required'
+        ],
+        titleRules: [
+          v => !!v || 'Title is required'
+        ]
+      }
+    },
+    computed: {
+      formIsValid () {
+        return this.category_id !== '' && this.category_title !== ''
+      }
+    },
+    methods: {
+      signup: function () {
+        console.log(this.category_id)
+        console.log(this.category_title)
+        axios.post('http: //localhost:3002/admin/category/addCategory', {
+          category_id: this.category_id,
+          category_title: this.category_title
+        }).then(response => {
+          console.log(response.data)
+          localStorage.getItem('token')
+        }).catch(error => {
+          console.log('Error adding new category')
+          console.log(error)
+          console.log(error.status)
+          console.log(error.code)
+        })
+      }
+    }
+  }
+</script>
---
+++ src/components/admin/category/viewCategory.vue
@@ -0,0 +1,148 @@
+<template>
+  <v-container fluid="true">
+  <div>
+    <v-spacer></v-spacer>
+
+    <v-layout>
+      <v-toolbar card prominent color="blue">
+        <v-toolbar-title class="white--text offset-sm4 ">Categories details</v-toolbar-title>
+          <v-spacer></v-spacer>
+      </v-toolbar>
+    </v-layout>
+    <v-layout>
+      <v-flex>
+
+        <v-card>
+          <v-card-title>
+            Categories
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="categories"
+      :search="search"
+      hide-actions
+      class="elevation-1"
+    >
+
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.category_id }}</td>
+        <td>{{ props.item.category_title }}</td>
+        <td class="justify-left layout px-4">
+          <v-btn icon class="mx-0" @click="editItem(props.item.category_id)">
+            <v-icon color="teal">edit</v-icon>
+          </v-btn>
+          <v-btn icon class="mx-0" @click="deleteItem(props.item)">
+            <v-icon color="pink">delete</v-icon>
+          </v-btn>
+        </td>
+      </template>
+      <v-alert slot="no-results" :value="true" color="error" icon="warning">
+        Your search for "{{ search }}" found no results.
+      </v-alert>
+
+      <template slot="pageText" slot-scope="{ pageStart, pageStop }">
+        From {{ pageStart }} to {{ pageStop }}
+      </template>
+    </v-data-table>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
+  </v-container>
+</template>
+
+
+
+<script>
+  export default {
+    data: () => ({
+      dialog: false,
+      search: '',
+      headers: [
+        {
+          text: 'Category Id', value: 'category_id'},
+        {text: 'Category Title', value: 'calories'},
+        {text: 'Actions', value: 'category_title'}
+      ],
+      editedIndex: 0,
+      editedItem: {
+        category_id: '',
+        category_title: 0
+      },
+      defaultItem: {
+        category_id: '',
+        category_title: 0
+      }
+    }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      }
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      }
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        this.categories = [
+          {
+            category_title: 'Frozen Yogurt',
+            category_id: 159
+          },
+          {
+            category_title: 'Ice cream sandwich',
+            category_id: 237
+          },
+          {
+            category_title: 'Eclair',
+            category_id: 262
+          },
+          {
+            category_title: 'Cupcake',
+            category_id: 305
+          },
+          {
+            category_title: 'Gingerbread',
+            category_id: 356
+          },
+          {
+            category_title: 'Jelly bean',
+            category_id: 375
+          },
+          {
+            category_title: 'Lollipop',
+            category_id: 392
+          }
+        ]
+      },
+
+      editItem (item) {
+        this.editedIndex = this.categories.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        const index = this.categories.indexOf(item)
+        confirm('Are you sure you want to delete this item?') && this.categories.splice(index, 1)
+      }
+    }
+  }
+</script>
